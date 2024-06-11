d4ee0b04f6c83d6ac28812bf6edc4b672b151fb5
Ignarrman <nacho.arromant@gmail.com>
2024-06-11 17:48:34 +0200

    feat: Verificación de email en SignUp + Redo SignUp

--- app/users/urls.py
+++ app/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 from django.contrib.auth import views as auth_views
 
-from app.users.views import ChangePasswordView, CustomPasswordResetView, UserListView, UserDetailView, \
+from app.users.views import ChangePasswordView, ConfirmEmailView, CreateUserView, CustomPasswordResetView, LoginView, UserListView, UserDetailView, \
     UserUpdateAndDestroyView, LoggedInUserView, CustomPasswordSendedView, CustomPasswordSuccesView
 
 router = SimpleRouter()
@@ -14,6 +14,7 @@ router.register(r'users/list', UserListView, basename="user-list")
 router.register(r'user', UserDetailView, basename="user-modify")
 router.register(r'users', UserUpdateAndDestroyView, basename="user-update-destroy")
 router.register(r'users/logged', LoggedInUserView, basename="user-logged")
+router.register(r'create_user', CreateUserView, basename="user-create")
 
 urlpatterns = [
     path('', include(router.urls)),
@@ -21,4 +22,6 @@ urlpatterns = [
     path('password-sended/',CustomPasswordSendedView.as_view(),name='request_password_reset'),
     path('password-success/',CustomPasswordSuccesView.as_view(),name='request_password_reset'),
     path('change-password/<str:uidb64>/<str:token>/',ChangePasswordView.as_view() , name='change_password'),
+    path('email-verified-success/<str:uidb64>/<str:token>/',ConfirmEmailView.as_view() , name='verification_success'),
+    path('login/',LoginView.as_view({'post':'post'}),name='login'),
     ]
