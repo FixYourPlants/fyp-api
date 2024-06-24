af9997f20f8433f4e8950e3becb3dd2eb3f7428f
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-06-24 20:05:27 +0200

    fix: Creating environments

--- app/config/production.py
+++ app/config/production.py
@@ -32,14 +32,8 @@ class Production(Common):
     # CORS settings
     CORS_ALLOW_ALL_ORIGINS = True
     CORS_ALLOW_CREDENTIALS = True
-    CORS_ALLOWED_ORIGINS = [
-        i
-        for i in list(config('CORS_ALLOWED_ORIGINS', default='*').split(','))
-        if i not in ['*', '']
-    ]
-    CORS_TRUSTED_ORIGINS = [
-        i
-        for i in list(config('CORS_ALLOWED_ORIGINS', default='*').split(','))
-        if i not in ['*', '']
-    ]
+    CORS_ALLOWED_ORIGINS = [i for i in list(config('CORS_ALLOWED_ORIGINS', default='*').split(',')) if i not in ['*', '']]
+    CORS_TRUSTED_ORIGINS = [i for i in list(config('CORS_ALLOWED_ORIGINS', default='*').split(',')) if i not in ['*', '']]
+    if config('CRACK', default='False') == 'True':
+        raise Exception(CORS_ALLOWED_ORIGINS)
 
