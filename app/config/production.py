e644fb19e687cd8b5be7e381608ac4196a6bc2bf
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-06-24 19:52:09 +0200

    fix: Creating environments

--- app/config/production.py
+++ app/config/production.py
@@ -37,6 +37,9 @@ class Production(Common):
         for i in list(config('CORS_ALLOWED_ORIGINS', default='*').split(','))
         if i not in ['*', '']
     ]
-
-    raise NotImplementedError(CORS_ALLOWED_ORIGINS)
+    CORS_TRUSTED_ORIGINS = [
+        i
+        for i in list(config('CORS_ALLOWED_ORIGINS', default='*').split(','))
+        if i not in ['*', '']
+    ]
 
