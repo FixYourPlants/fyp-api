2b5c4b065d0bf2ccad0c92e2f79cbae6608661e5
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-07-25 22:32:40 +0200

    fix: Delete nose

--- app/diary/tests/test_diary_serializers.py
+++ app/diary/tests/test_diary_serializers.py
@@ -1,6 +1,5 @@
 from django.forms.models import model_to_dict
 from django.test import TestCase
-from nose.tools import eq_
 
 from .factories import DiaryFactory, PageFactory
 from ..serializers import DiarySerializer, DiaryCreateSerializer, PageSerializer, PageCreateSerializer
@@ -20,7 +19,7 @@ class TestDiarySerializer(TestCase):
 
     def test_serializer_with_empty_data(self):
         serializer = DiarySerializer(data={})
-        eq_(serializer.is_valid(), False)
+        self.assertFalse(serializer.is_valid())
 
     def test_serializer_with_valid_data(self):
         serializer = DiaryCreateSerializer(data=self.diary_data)
