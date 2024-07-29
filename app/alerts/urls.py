c6e333d1c1f79b0760c50eb33877987c767bbb77
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-07-29 12:38:55 +0200

    fix: Error in imports

--- app/alerts/urls.py
+++ app/alerts/urls.py
@@ -1,3 +1,4 @@
+from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
 from app.alerts.views import AlertListViewGov
@@ -7,3 +8,7 @@ router = SimpleRouter()
 ALERT
 '''
 router.register(r'alerts/list/gob', AlertListViewGov, basename="alert-list-gob")
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
